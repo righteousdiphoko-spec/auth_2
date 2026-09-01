4e3d4e8eb015eec705a804c7d0893ef0a3e5392d
righteousdiphoko-spec <righteousdiphoko@gmail.com>
2026-09-01 15:26:15 +0200

    Fix Vercel serverless startup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,9 +3,19 @@ import { env } from "./config/env.js";
 import prisma from "./config/prisma.js";
 import { seedDemoUser } from "./config/store.js";
 
-await prisma.$connect();
-await seedDemoUser();
+const startServer = async () => {
+  await prisma.$connect();
+  await seedDemoUser();
 
-const server = app.listen(env.PORT, () => console.log(`Auth2 API listening on ${env.PORT}`));
-const shutdown = () => { server.close(() => process.exit(0)); };
-process.on("SIGTERM", shutdown); process.on("SIGINT", shutdown);
+  const server = app.listen(env.PORT, () => console.log(`Auth2 API listening on ${env.PORT}`));
+  const shutdown = () => { server.close(() => process.exit(0)); };
+  process.on("SIGTERM", shutdown);
+  process.on("SIGINT", shutdown);
+  return server;
+};
+
+if (!process.env.VERCEL) {
+  startServer();
+}
+
+export default app;
